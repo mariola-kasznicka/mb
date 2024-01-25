deffbc0dccdd3d72423c99f4bbf9bdb00405b572
Mariola Kasznicka <mariolakasznicka@Mariola-MBP.local>
2024-01-25 19:03:05 +0100

    Style update

--- public/js/main.js
+++ public/js/main.js
@@ -5,3 +5,33 @@ $(".footer-menu-item" ).on("click", function() {
 $("#mobileMenuToggler" ).on("click", function() {
     $("body").toggleClass("no-scroll");
 });
+
+$(document).ready(function(){
+    initBxSlider()
+
+    $(window).on( "resize", function() {
+        initBxSlider()
+    });
+});
+
+function initBxSlider(){
+    if($(window).width() >= 1280) {
+        $('.bxslider.mb-slider').bxSlider({
+            pager: false,
+            minSlides: 4,
+            maxSlides: 4,
+            slideWidth: 5000,
+            slideMargin: 32,
+            moveSlides: 1,
+            nextText: '',
+            prevText: '',
+            onSliderResize: function() {
+                if($(window).width() < 1280) {
+                    this.destroySlider()
+                }
+
+            }
+        });
+    }
+}
+
